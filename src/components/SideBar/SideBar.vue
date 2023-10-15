477feed8210b8778cd28194ddde58ff536009d95
niko_ <nikolaj.aleksejenkov@gmail.com>
2023-10-15 23:51:00 +0300

    togglable sidebar : - added logic to retract and expand the sidebar

--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="w-64 h-screen bg-zinc-900 rounded-r-md flex flex-col">
-        <div class="px-6 pt-8">
-            <div class="flex items-center justify-between">
+    <div class="h-screen bg-zinc-900 rounded-r-md flex flex-col"
+        :class="expanded ? 'sidebar-expanded' : 'sidebar-retracted'">
+        <div :class="expanded ? 'button-container-expanded' : 'button-container-retracted'">
+            <div class="flex items-center justify-between" :class="expanded ? '' : 'flex-col'">
                 <RouterLink to="/">
                     <a class="bg-green-600 ml-1 p-1.5 rounded flex items-center justify-center">
                         <fa icon="bug" class="h-5" />
                     </a>
                 </RouterLink>
-                <button class="flex items-center justify-center p-0.5 rounded bg-zinc-800 h-6 w-6">
-                    <fa icon="angles-left" class="text-green-500" />
+                <button class="flex items-center justify-center p-0.5 h-6 w-6" @click="toggleSideBar">
+                    <fa :icon="expanded ? 'angles-left' : 'angles-right'" class="text-green-500" />
                 </button>
             </div>
         </div>
@@ -18,32 +19,70 @@
         </div>
 
         <div class="px-6 pt-4">
-            <ul class="flex flex-col space-y-2 pl-0.5">
-                <SideBarItem to="/budget" icon="coins" name="Budget" />
-                <SideBarItem to="/planner" icon="clipboard" name="Planner" />
-                <SideBarItem to="/debts" icon="receipt" name="Debts" />
+            <ul class="flex flex-col space-y-2" :class="expanded ? 'list-group-expanded' : ''">
+                <SideBarItem :class="!expanded ? 'sidebar-item-retractred' : ''" to="/budget" icon="coins" :name="expanded ? 'Budget' : ''" />
+                <SideBarItem :class="!expanded ? 'sidebar-item-retractred' : ''" to="/planner" icon="clipboard" :name="expanded ? 'Planner' : ''" />
+                <SideBarItem :class="!expanded ? 'sidebar-item-retractred' : ''" to="/debts" icon="receipt" :name="expanded ? 'Debts' : ''" />
             </ul>
         </div>
 
         <div class="mt-auto pb-4">
             <div class="px-6 pt-8">
-            <hr class="border-zinc-700" />
-        </div>
+                <hr class="border-zinc-700" />
+            </div>
 
-        <div class="px-6 pt-4">
-            <ul class="flex flex-col space-y-2 pl-0.5">
-                <SideBarItem to="/settings" icon="gear" name="Settings" />
-            </ul>
-        </div>
+            <div class="px-6 pt-4">
+                <ul class="flex flex-col space-y-2" :class="expanded ? 'list-group-expanded' : ''">
+                    <SideBarItem to="/settings" icon="gear" :name="expanded ? 'Settings' : ''" />
+                </ul>
+            </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+.sidebar-expanded {
+    width: 12rem;
+}
+
+.sidebar-retracted {
+    width: 4rem;
+}
+
+.button-container-expanded {
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+    padding-top: 2rem;
+}
+
+.button-container-retracted {
+    padding-top: 1.5rem;
+}
+
+.list-group-expanded {
+    padding-left: 0.125rem;
+}
+
+.sidebar-item-retractred {
+    padding-top: 0.5rem;
+}
+</style>
 
 <script>
 import SideBarItem from './SideBarItem.vue';
 
 export default {
     name: 'SideBar',
+    data() {
+        return {
+            expanded: true
+        }
+    },
+    methods: {
+        toggleSideBar() {
+            this.expanded = !this.expanded;
+        }
+    },
     components: {
         SideBarItem
     }
--- src/components/SideBar/SideBarItem.vue
+++ src/components/SideBar/SideBarItem.vue
@@ -1,20 +1,20 @@
 <template>
     <RouterLink :to="to">
-        <li class="relative text-zinc-500">
-            <div class="absolute inset-y-0 left-0 flex items-center pl-2">
+        <li class="relative text-zinc-500 hover:bg-zinc-800 rounded">
+            <div class="absolute inset-y-0 left-0 flex items-center">
                 <fa :icon="icon" class="w-5 h-5" />
             </div>
-            <a class="inline-block w-full px-4 py-2 pl-8 hover:bg-zinc-800 rounded">{{ name }}</a>
+            <a class="inline-block w-full px-4 py-2 pl-7 ">{{ name }}</a>
         </li>
     </RouterLink>
 </template>
 
 <script>
-    export default {
-        props: {
-            to: String,
-            icon: String,
-            name: String
-        }
+export default {
+    props: {
+        to: String,
+        icon: String,
+        name: String
     }
+}
 </script>
