efc27d67b0bcd9c90df78cc93730d83ed2a45e35
Nikolajs Aleksejenkovs <nikolaj.aleksejenkov@gmail.com>
2023-10-16 20:43:20 +0300

    Revert "separated the frontend to separate folder"
    This reverts commit 873b5de72574b5be7980a5d0126182cfbc02bc1b.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex">
+    <SideBar />
+    <RouterView />
+  </div>
+</template>
+
+<script>
+import SideBar from './components/SideBar/SideBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SideBar
+  }
+}
+</script>
---
+++ src/components/SideBar/SideBarItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <RouterLink :to="to">
+        <li class="flex text-zinc-500 hover:bg-zinc-800 hover:text-green-500 rounded" :class="!expanded && 'py-2'">
+            <div class="flex pl-5">
+                <div class="flex items-center">
+                    <fa :icon="icon" class="w-6 h-6" />
+                </div>
+                <a class="inline-block w-full px-4 py-2" :class="expanded ? 'text-fade-in' : 'text-fade-out'">{{ label }}</a>
+            </div>
+        </li>
+    </RouterLink>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+    border-right: 5px solid rgb(34 197 94);
+    background-color: rgb(39 39 42)
+}
+
+.text-fade-out {
+    opacity: 0;
+    transition: 0.3s ease-out;
+}
+.text-fade-in {
+    opacity: 100%;
+    transition: 0.3s ease-out;
+}
+</style>
+
+<script>
+export default {
+    props: {
+        to: String,
+        icon: String,
+        label: String,
+        expanded: Boolean
+    }
+}
+</script>
