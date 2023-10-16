fd7f2befdf0927bea0648c27a76ec234b629b3fa
niko_ <nikolaj.aleksejenkov@gmail.com>
2023-10-16 20:01:37 +0300

    sidebar changes: - changed sidebar styles - added bg color and border on sideBarItem when active - added animations when sidebar expands or retracts

--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="h-screen bg-zinc-900 rounded-r-md flex flex-col"
-        :class="expanded ? 'sidebar-expanded' : 'sidebar-retracted'">
-        <div :class="expanded ? 'button-container-expanded' : 'button-container-retracted'">
-            <div class="flex items-center justify-between" :class="expanded ? '' : 'flex-col'">
+    <div class="h-screen bg-zinc-900 rounded-r-md flex flex-col sidebar-animation" :class="expanded ? 'w-48' : 'w-16'">
+        <div :class="expanded ? 'px-6 pt-8' : 'pt-6'">
+            <div class="flex items-center justify-between" :class="!expanded && 'flex-col'">
                 <RouterLink to="/">
                     <a class="bg-green-600 ml-1 p-1.5 rounded flex items-center justify-center">
                         <fa icon="bug" class="h-5" />
                     </a>
                 </RouterLink>
-                <button class="flex items-center justify-center p-0.5 h-6 w-6" @click="toggleSideBar">
-                    <fa :icon="expanded ? 'angles-left' : 'angles-right'" class="text-green-500" />
+                <button class="flex items-center justify-center p-0.5 h-6 w-6"
+                    :class="{ 'mt-4 button-animation-right': !expanded, 'button-animation-left': expanded }"
+                    @click="toggleSideBar">
+                    <fa :icon="expanded ? 'angles-left' : 'angles-right'" class="text-green-500 hover:text-green-300 p-2" />
                 </button>
             </div>
         </div>
@@ -18,11 +19,11 @@
             <hr class="border-zinc-700" />
         </div>
 
-        <div class="px-6 pt-4">
-            <ul class="flex flex-col space-y-2" :class="expanded ? 'list-group-expanded' : ''">
-                <SideBarItem :class="!expanded ? 'sidebar-item-retractred' : ''" to="/budget" icon="coins" :name="expanded ? 'Budget' : ''" />
-                <SideBarItem :class="!expanded ? 'sidebar-item-retractred' : ''" to="/planner" icon="clipboard" :name="expanded ? 'Planner' : ''" />
-                <SideBarItem :class="!expanded ? 'sidebar-item-retractred' : ''" to="/debts" icon="receipt" :name="expanded ? 'Debts' : ''" />
+        <div class="pt-4">
+            <ul class="flex flex-col space-y-2">
+                <SideBarItem to="/budget" icon="coins" :label="expanded ? 'Budget' : ''" :expanded="expanded" />
+                <SideBarItem to="/planner" icon="clipboard" :label="expanded ? 'Planner' : ''" :expanded="expanded" />
+                <SideBarItem to="/debts" icon="receipt" :label="expanded ? 'Debts' : ''" :expanded="expanded" />
             </ul>
         </div>
 
@@ -31,9 +32,9 @@
                 <hr class="border-zinc-700" />
             </div>
 
-            <div class="px-6 pt-4">
-                <ul class="flex flex-col space-y-2" :class="expanded ? 'list-group-expanded' : ''">
-                    <SideBarItem to="/settings" icon="gear" :name="expanded ? 'Settings' : ''" />
+            <div class="pt-2">
+                <ul class="flex flex-col space-y-2">
+                    <SideBarItem to="/settings" icon="gear" :label="expanded ? 'Settings' : ''" :expanded="expanded"/>
                 </ul>
             </div>
         </div>
@@ -41,33 +42,26 @@
 </template>
 
 <style scoped>
-.sidebar-expanded {
-    width: 12rem;
+.sidebar-animation {
+    transition: 0.2s ease-out;
 }
 
-.sidebar-retracted {
-    width: 4rem;
+.button-animation-left {
+    transition: 0.2s ease-out;
+
+    &:hover {
+        transform: translateX(-0.5rem);
+    }
 }
 
-.button-container-expanded {
-    padding-left: 1.5rem;
-    padding-right: 1.5rem;
-    padding-top: 2rem;
-}
+.button-animation-right {
+    transition: 0.2s ease-out;
 
-.button-container-retracted {
-    padding-top: 1.5rem;
-}
-
-.list-group-expanded {
-    padding-left: 0.125rem;
-}
-
-.sidebar-item-retractred {
-    padding-top: 0.5rem;
+    &:hover {
+        transform: translateX(0.5rem);
+    }
 }
 </style>
-
 <script>
 import SideBarItem from './SideBarItem.vue';
 
